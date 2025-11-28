2697b3f901bea65da95ca5b3b94e4673a4e6d25a
mayahyah <24-02357@g.batstate-u.edu.ph>
2025-11-28 23:09:36 +0800

    Update GameLevels.java

--- GameLevels.java
+++ GameLevels.java
@@ -5,7 +5,7 @@ class GameLevels {
     private Player player;
     private Scanner scanner = new Scanner(System.in);
 
-    // Colors 
+    // ANSI COLORS
     private static final String RESET = "\u001B[0m";
     private static final String RED = "\u001B[31m";
     private static final String GREEN = "\u001B[32m";
@@ -17,7 +17,9 @@ class GameLevels {
         this.player = player;
     }
 
-    // Clear Screen Method
+    // ==========================
+    // CLEAR SCREEN METHOD
+    // ==========================
     public static void clearScreen() {
         try {
             if (System.getProperty("os.name").contains("Windows")) {
@@ -31,7 +33,9 @@ class GameLevels {
         }
     }
 
-    // Level Selection Menu
+    // ==========================
+    // LEVEL SELECTION MENU
+    // ==========================
     public void showLevelMenu() {
         while (true) {
 
@@ -106,7 +110,9 @@ class GameLevels {
         }
     }
 
-    // Scientist Selection
+    // ==========================
+    // SCIENTIST SELECTION
+    // ==========================
     private Scientists selectScientist() {
         while (true) {
             clearScreen();
@@ -129,25 +135,61 @@ class GameLevels {
 
             switch (c) {
                 case 1:
+                    clearScreen();
+                    System.out.println(GREEN + "Special Ability Activated:\n");
+                    System.out.println(GREEN + "     ▀█▀ ██▀ █   ██▀ ▄▀▀ ▄▀▀ ▄▀▄ █▀▄ ██▀   ▄▀▄ █▀   ▀█▀ █▀▄ █ █ ▀█▀ █▄█\r\n" + //
+                                                "      █  █▄▄ █▄▄ █▄▄ ▄██ ▀▄▄ ▀▄▀ █▀  █▄▄   ▀▄▀ █▀    █  █▀▄ ▀▄█  █  █ █\r" + //
+                                                "");
+                    System.out.println(GREEN + "\"As the great Galileo Galilei, you will receive hints for astronomy questions!\"" + RESET);
                     pressEnter();
                     return new Scientists("Galileo Galilei", 50, Scientists.Ability.HINT,
                             "Provides hints for astronomy questions.");
                 case 2:
+                    clearScreen();
+                    System.out.println(GREEN + "Special Ability Activated:\n");
+                    System.out.println(GREEN + "             █▄ █ ▄▀▄ ▀█▀ █ █ █▀▄ ██▀ ▀ ▄▀▀     █ █ █ █▀▄ ▄▀  ██▀\r\n" + //
+                                                "             █ ▀█ █▀█  █  ▀▄█ █▀▄ █▄▄  ▄██    ▀▄█ ▀▄█ █▄▀ ▀▄█ █▄▄\r" + //
+                                                "");
+                    System.out.println(GREEN + "\"With excellent mastery of physics, you can skip physics questions twice per level!\"" + RESET);
                     pressEnter();
                     return new Scientists("Isaac Newton", 50, Scientists.Ability.SKIP_QUESTION,
                             "Can skip physics questions (2x per level).");
                 case 3:
+                    clearScreen();
+                    System.out.println(GREEN + "Special Ability Activated:\n");
+                    System.out.println(GREEN + "                  ██▀ █ █ ▄▀▄ █   █ █ ▀█▀ █ ▄▀▄ █▄ █ ▄▀▄ █▀▄ ▀▄▀\r\n" + //
+                                                "                  █▄▄ ▀▄▀ ▀▄▀ █▄▄ ▀▄█  █  █ ▀▄▀ █ ▀█ █▀█ █▀▄  █ \r" + //
+                                                "");
+                    System.out.println(GREEN + "\"The power of evolution grants you to copy another scientist's ability each question!\"" + RESET);
                     pressEnter();
-                    return new Darwin(); 
+                    return new Darwin(); // <── Darwin polymorphism
                 case 4:
+                    clearScreen();
+                    System.out.println(GREEN + "Special Ability Activated:\n");
+                    System.out.println(GREEN + "     ██▄ █▀▄ ▄▀▄ █ █▄ █   ▄▀▄ █▀   ▀█▀ ▄▀▄ █▄ ▄█ ▄▀▄ █▀▄ █▀▄ ▄▀▄ █   █\r\n" + //
+                                                "     █▄█ █▀▄ █▀█ █ █ ▀█   ▀▄▀ █▀    █  ▀▄▀ █ ▀ █ ▀▄▀ █▀▄ █▀▄ ▀▄▀ ▀▄▀▄▀\r" + //
+                                                "");
+                    System.out.println(GREEN + "\"The number of questions are decreased by 5 as you gain the power of Freud!\"" + RESET);
                     pressEnter();
                     return new Scientists("Sigmund Freud", 50, Scientists.Ability.REDUCE_QUESTION,
                             "Reduces the total number of questions by 5.");
                 case 5:
+                    clearScreen();
+                    System.out.println(GREEN + "Special Ability Activated:\n");
+                    System.out.println(GREEN + "             █▀▄ ▄▀▄ █▀▄ █ ▄▀▄ █▄ █ ▄▀▀ ██▀   ▄▀▀ █ █ █▀▄ █ ▄▀▄ █ █ ▄▀▀ █ ▀█▀ ▀▄▀\r\n" + //
+                                                "             █▀▄ █▀█ █▄▀ █ █▀█ █ ▀█ ▀▄▄ █▄▄   ▀▄▄ ▀▄█ █▀▄ █ ▀▄▀ ▀▄█ ▄██ █  █   █ \r" + //
+                                                "");
+                    System.out.println(GREEN + "\"With Marie Curie's brilliance and curiousity, you gain +30 health at the start of each level!\"" + RESET);
                     pressEnter();
                     return new Scientists("Marie Curie", 50, Scientists.Ability.EXTRA_LIFE,
                             "Adds +30 health at the start of each level.");
                 case 6:
+                    clearScreen();
+                    System.out.println(GREEN + "Special Ability Activated:\n");
+                    System.out.println(GREEN + "                    █   ▄▀▄ █ █ ██▀   █   ▄▀▄ ▄▀▄ █▀▄ ▄▀▀\r\n" + //
+                                                "                    █▄▄ ▀▄▀ ▀▄▀ █▄▄   █▄▄ ▀▄▀ ▀▄▀ █▀  ▄██\r" + //
+                                                ""); 
+                    System.out.println(GREEN + "\"With Ada Lovelace's intellect, you heal 5 health for every correct answer!\"" + RESET);
                     pressEnter();
                     return new Scientists("Ada Lovelace", 50, Scientists.Ability.HEAL,
                             "Heals +5 health on each correct answer.");
@@ -158,7 +200,9 @@ class GameLevels {
         }
     }
 
-    // Quiz bee Start
+    // ==========================
+    // PLAY LEVEL
+    // ==========================
     public boolean playLevel(int level) {
 
         clearScreen();
@@ -167,7 +211,18 @@ class GameLevels {
         s.resetPerLevelFlags();
 
         int questionCount = (level == 5 || level == 10 ? 20 : 10);
-
+        if (level == 5 || level == 10) {
+            clearScreen();
+            printlnC(RED, "▄▄▄▄·       .▄▄ · .▄▄ ·   ▄▄▌  ▄▄▄ . ▌ ▐·▄▄▄ .▄▄▌  \r\n" + //
+                                    "▐█ ▀█▪ ▄█▀▄ ▐█ ▀. ▐█ ▀.   ██•  ▀▄.▀·▪█·█▌▀▄.▀·██•  \r\n" + //
+                                    "▐█▀▀█▄▐█▌.▐▌▄▀▀▀█▄▄▀▀▀█▄  ██ ▪ ▐▀▀▪▄▐█▐█•▐▀▀▪▄██ ▪ \r\n" + //
+                                    "██▄▪▐█▐█▌.▐▌▐█▄▪▐█▐█▄▪▐█  ▐█▌ ▄▐█▄▄▌ ███ ▐█▄▄▌▐█▌ ▄\r\n" + //
+                                    "·▀▀▀▀  ▀█▄▀▪ ▀▀▀▀  ▀▀▀▀   .▀▀▀  ▀▀▀ . ▀   ▀▀▀ .▀▀▀ \r" + //
+                                    "");
+            System.out.println(RED + "   OOPs! Give it your best shot. You got this!" + RESET);
+            pressEnter();
+        }
+        
         if (s.getAbility() == Scientists.Ability.REDUCE_QUESTION)
             questionCount = Math.max(5, questionCount - 5);
 
@@ -256,7 +311,7 @@ class GameLevels {
                     player.addScore(10);
 
                     // Ada Lovelace heal effect
-                    if (s.getAbility() == Scientists.Ability.HEAL) {
+                    if (s.getAbility() == Scientists.Ability.HEAL || copied == Scientists.Ability.HEAL) {
                         s.modifyHealth(+5);
                     }
 
@@ -273,7 +328,9 @@ class GameLevels {
                     continue;
                 }
 
-                // If wrong answer...
+                // ==========================
+                // WRONG ANSWER HANDLING
+                // ==========================
                 printlnC(RED, "\nWrong Answer! Health -10");
                 s.modifyHealth(-10);
 
@@ -292,7 +349,9 @@ class GameLevels {
         return true;
     }
 
-    // Error Handling
+    // ==========================
+    // INPUT + UTILITY
+    // ==========================
     private int readInt() {
         while (true) {
             try {
@@ -313,4 +372,3 @@ class GameLevels {
         System.out.println(color + text + RESET);
     }
 }
-
